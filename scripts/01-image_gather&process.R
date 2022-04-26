028ba8f0f618ddbb7a435bec1d80b72076192b54
Sampson Zhao <sampson.zhao@mail.utoronto.ca>
2022-04-25 22:27:01 -0400

    Edit Commit

--- scripts/01-image_gather&process.R
+++ scripts/01-image_gather&process.R
@@ -253,6 +253,8 @@ close(b2_5_uc)
 # processed photo OCR and export to text
 b2_1_cleaned <- image_read(here::here("inputs/data/Book2/images/book2_65.jpg")) %>%
   image_resize("2000") %>%
+  image_modulate(brightness = 125) %>%
+  image_contrast(sharpen = 2) %>%
   image_convert(colorspace = 'gray') %>%
   image_trim() %>%
   image_ocr()
@@ -261,45 +263,53 @@ b2_1_c<-file(here::here('inputs/data/Book2/b2_1_clean.txt'))
 writeLines(b2_1_cleaned, b2_1_c, useBytes = TRUE)
 close(b2_1_c)
 
-b1_2_cleaned <- image_read(here::here("inputs/data/Book1/images/book1_259_1_crop.jpg")) %>%
+b2_2_cleaned <- image_read(here::here("inputs/data/Book2/images/book2_127.jpg")) %>%
   image_resize("2000") %>%
+  image_modulate(brightness = 125) %>%
+  image_contrast(sharpen = 2) %>%
   image_convert(colorspace = 'gray') %>%
   image_trim() %>%
   image_ocr()
 
-b1_2_c<-file(here::here('inputs/data/Book1/b1_2_clean.txt'))
-writeLines(b1_2_cleaned, b1_2_c, useBytes = TRUE)
-close(b1_2_c)
+b2_2_c<-file(here::here('inputs/data/Book2/b2_2_clean.txt'))
+writeLines(b2_2_cleaned, b2_2_c, useBytes = TRUE)
+close(b2_2_c)
 
-b1_3_cleaned <- image_read(here::here("inputs/data/Book1/images/book1_493_2_crop.jpg")) %>%
+b2_3_cleaned <- image_read(here::here("inputs/data/Book2/images/book2_183.jpg")) %>%
   image_resize("2000") %>%
+  image_modulate(brightness = 125) %>%
+  image_contrast(sharpen = 2) %>%
   image_convert(colorspace = 'gray') %>%
   image_trim() %>%
   image_ocr()
 
-b1_3_c<-file(here::here('inputs/data/Book1/b1_3_clean.txt'))
-writeLines(b1_3_cleaned, b1_3_c, useBytes = TRUE)
-close(b1_3_c)
+b2_3_c<-file(here::here('inputs/data/Book2/b2_3_clean.txt'))
+writeLines(b2_3_cleaned, b2_3_c, useBytes = TRUE)
+close(b2_3_c)
 
-b1_4_cleaned <- image_read(here::here("inputs/data/Book1/images/book1_613_2_crop.jpg")) %>%
+b2_4_cleaned <- image_read(here::here("inputs/data/Book2/images/book2_235.jpg")) %>%
   image_resize("2000") %>%
+  image_modulate(brightness = 125) %>%
+  image_contrast(sharpen = 2) %>%
   image_convert(colorspace = 'gray') %>%
   image_trim() %>%
   image_ocr()
 
-b1_4_c<-file(here::here('inputs/data/Book1/b1_4_clean.txt'))
-writeLines(b1_4_cleaned, b1_4_c, useBytes = TRUE)
-close(b1_4_c)
+b2_4_c<-file(here::here('inputs/data/Book2/b2_4_clean.txt'))
+writeLines(b2_4_cleaned, b2_4_c, useBytes = TRUE)
+close(b2_4_c)
 
-b1_5_cleaned <- image_read(here::here("inputs/data/Book1/images/book1_1099_2_crop.jpg")) %>%
+b2_5_cleaned <- image_read(here::here("inputs/data/Book2/images/book2_318.jpg")) %>%
   image_resize("2000") %>%
+  image_modulate(brightness = 125) %>%
+  image_contrast(sharpen = 2) %>%
   image_convert(colorspace = 'gray') %>%
   image_trim() %>%
   image_ocr()
 
-b1_5_c<-file(here::here('inputs/data/Book1/b1_5_clean.txt'))
-writeLines(b1_5_cleaned, b1_5_c, useBytes = TRUE)
-close(b1_5_c)
+b2_5_c<-file(here::here('inputs/data/Book2/b2_5_clean.txt'))
+writeLines(b2_5_cleaned, b2_5_c, useBytes = TRUE)
+close(b2_5_c)
 
 # advanced processed photo OCR and export to text
 
@@ -312,58 +322,59 @@ b2_1_a<-file(here::here('inputs/data/Book2/b2_1_adv.txt'))
 writeLines(b2_1_adv, b2_1_a, useBytes = TRUE)
 close(b2_1_a)
 
-b1_2_adv <- image_read(here::here("inputs/data/Book1/images/book1_259_adv.jpg")) %>%
+b2_2_adv <- image_read(here::here("inputs/data/Book2/images/book2_127_adv.jpg")) %>%
   image_resize("2000") %>%
   image_trim() %>%
   image_ocr()
 
-b1_2_a<-file(here::here('inputs/data/Book1/b1_2_adv.txt'))
-writeLines(b1_2_adv, b1_2_a, useBytes = TRUE)
-close(b1_2_a)
+b2_2_a<-file(here::here('inputs/data/Book2/b2_2_adv.txt'))
+writeLines(b2_2_adv, b2_2_a, useBytes = TRUE)
+close(b2_2_a)
 
-b1_3_adv <- image_read(here::here("inputs/data/Book1/images/book1_493_adv.jpg")) %>%
+b2_3_adv <- image_read(here::here("inputs/data/Book2/images/book2_183_adv.jpg")) %>%
   image_resize("2000") %>%
   image_trim() %>%
   image_ocr()
 
-b1_3_a<-file(here::here('inputs/data/Book1/b1_3_adv.txt'))
-writeLines(b1_3_adv, b1_3_a, useBytes = TRUE)
-close(b1_3_a)
+b2_3_a<-file(here::here('inputs/data/Book2/b2_3_adv.txt'))
+writeLines(b2_3_adv, b2_3_a, useBytes = TRUE)
+close(b2_3_a)
 
-b1_4_adv <- image_read(here::here("inputs/data/Book1/images/book1_613_adv.jpg")) %>%
+b2_4_adv <- image_read(here::here("inputs/data/Book2/images/book2_235_adv.jpg")) %>%
   image_resize("2000") %>%
   image_trim() %>%
   image_ocr()
 
-b1_4_a<-file(here::here('inputs/data/Book1/b1_4_adv.txt'))
-writeLines(b1_4_adv, b1_4_a, useBytes = TRUE)
-close(b1_4_a)
+b2_4_a<-file(here::here('inputs/data/Book2/b2_4_adv.txt'))
+writeLines(b2_4_adv, b2_4_a, useBytes = TRUE)
+close(b2_4_a)
 
-b1_5_adv <- image_read(here::here("inputs/data/Book1/images/book1_1099_2_crop.jpg")) %>%
+b2_5_adv <- image_read(here::here("inputs/data/Book2/images/book2_318_adv.jpg")) %>%
   image_resize("2000") %>%
   image_trim() %>%
   image_ocr()
 
-b1_5_a<-file(here::here('inputs/data/Book1/b1_5_adv.txt'))
-writeLines(b1_5_adv, b1_5_a, useBytes = TRUE)
-close(b1_5_a)
+b2_5_a<-file(here::here('inputs/data/Book2/b2_5_adv.txt'))
+writeLines(b2_5_adv, b2_5_a, useBytes = TRUE)
+close(b2_5_a)
+
 
 # Compare the two generated OCR files to original text visually
 
-comp_uc_1_1 <- diffr(here::here('inputs/data/Book1/b1_1_unclean.txt'), here::here('inputs/data/Book1/b1_1_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_uc_1_2 <- diffr(here::here('inputs/data/Book1/b1_2_unclean.txt'), here::here('inputs/data/Book1/b1_2_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_uc_1_3 <- diffr(here::here('inputs/data/Book1/b1_3_unclean.txt'), here::here('inputs/data/Book1/b1_3_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_uc_1_4 <- diffr(here::here('inputs/data/Book1/b1_4_unclean.txt'), here::here('inputs/data/Book1/b1_4_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_uc_1_5 <- diffr(here::here('inputs/data/Book1/b1_5_unclean.txt'), here::here('inputs/data/Book1/b1_5_original.txt'), contextSize = 0, minJumpSize = 500)
+comp_uc_2_1 <- diffr(here::here('inputs/data/Book2/b2_1_original.txt'), here::here('inputs/data/Book2/b2_1_unclean.txt'), contextSize = 0, minJumpSize = 500)
+comp_uc_2_2 <- diffr(here::here('inputs/data/Book2/b2_2_original.txt'), here::here('inputs/data/Book2/b2_2_unclean.txt'), contextSize = 0, minJumpSize = 500)
+comp_uc_2_3 <- diffr(here::here('inputs/data/Book2/b2_3_original.txt'), here::here('inputs/data/Book2/b2_3_unclean.txt'), contextSize = 0, minJumpSize = 500)
+comp_uc_2_4 <- diffr(here::here('inputs/data/Book2/b2_4_original.txt'), here::here('inputs/data/Book2/b2_4_unclean.txt'), contextSize = 0, minJumpSize = 500)
+comp_uc_2_5 <- diffr(here::here('inputs/data/Book2/b2_5_original.txt'), here::here('inputs/data/Book2/b2_5_unclean.txt'), contextSize = 0, minJumpSize = 500)
 
-comp_c_1_1 <- diffr(here::here('inputs/data/Book1/b1_1_clean.txt'), here::here('inputs/data/Book1/b1_1_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_c_1_2 <- diffr(here::here('inputs/data/Book1/b1_2_clean.txt'), here::here('inputs/data/Book1/b1_2_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_c_1_3 <- diffr(here::here('inputs/data/Book1/b1_3_clean.txt'), here::here('inputs/data/Book1/b1_3_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_c_1_4 <- diffr(here::here('inputs/data/Book1/b1_4_clean.txt'), here::here('inputs/data/Book1/b1_4_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_c_1_5 <- diffr(here::here('inputs/data/Book1/b1_5_clean.txt'), here::here('inputs/data/Book1/b1_5_original.txt'), contextSize = 0, minJumpSize = 500)
+comp_c_2_1 <- diffr(here::here('inputs/data/Book2/b2_1_original.txt'), here::here('inputs/data/Book2/b2_1_clean.txt'), contextSize = 0, minJumpSize = 500)
+comp_c_2_2 <- diffr(here::here('inputs/data/Book2/b2_2_original.txt'), here::here('inputs/data/Book2/b2_2_clean.txt'), contextSize = 0, minJumpSize = 500)
+comp_c_2_3 <- diffr(here::here('inputs/data/Book2/b2_3_original.txt'), here::here('inputs/data/Book2/b2_3_clean.txt'), contextSize = 0, minJumpSize = 500)
+comp_c_2_4 <- diffr(here::here('inputs/data/Book2/b2_4_original.txt'), here::here('inputs/data/Book2/b2_4_clean.txt'), contextSize = 0, minJumpSize = 500)
+comp_c_2_5 <- diffr(here::here('inputs/data/Book2/b2_5_original.txt'), here::here('inputs/data/Book2/b2_5_clean.txt'), contextSize = 0, minJumpSize = 500)
 
-comp_a_1_1 <- diffr(here::here('inputs/data/Book1/b1_1_adv.txt'), here::here('inputs/data/Book1/b1_1_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_a_1_2 <- diffr(here::here('inputs/data/Book1/b1_2_adv.txt'), here::here('inputs/data/Book1/b1_2_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_a_1_3 <- diffr(here::here('inputs/data/Book1/b1_3_adv.txt'), here::here('inputs/data/Book1/b1_3_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_a_1_4 <- diffr(here::here('inputs/data/Book1/b1_4_adv.txt'), here::here('inputs/data/Book1/b1_4_original.txt'), contextSize = 0, minJumpSize = 500)
-comp_a_1_5 <- diffr(here::here('inputs/data/Book1/b1_5_adv.txt'), here::here('inputs/data/Book1/b1_5_original.txt'), contextSize = 0, minJumpSize = 500)
+comp_a_2_1 <- diffr(here::here('inputs/data/Book2/b2_1_original.txt'), here::here('inputs/data/Book2/b2_1_adv.txt'), contextSize = 0, minJumpSize = 500)
+comp_a_2_2 <- diffr(here::here('inputs/data/Book2/b2_2_original.txt'), here::here('inputs/data/Book2/b2_2_adv.txt'), contextSize = 0, minJumpSize = 500)
+comp_a_2_3 <- diffr(here::here('inputs/data/Book2/b2_3_original.txt'), here::here('inputs/data/Book2/b2_3_adv.txt'), contextSize = 0, minJumpSize = 500)
+comp_a_2_4 <- diffr(here::here('inputs/data/Book2/b2_4_original.txt'), here::here('inputs/data/Book2/b2_4_adv.txt'), contextSize = 0, minJumpSize = 500)
+comp_a_2_5 <- diffr(here::here('inputs/data/Book2/b2_5_original.txt'), here::here('inputs/data/Book2/b2_5_adv.txt'), contextSize = 0, minJumpSize = 500)
--- scripts/02-percent_diff_generation.R
+++ scripts/02-percent_diff_generation.R
@@ -7,6 +7,10 @@
 # - reticulate::py_config() needs to be run at least once before fuzzywuzzyR 
 #   package can be used for text differences.
 
+# REQUIREMENT: please run reticulate::py_config() before loading the workspace 
+# setup packages, as fuzzywuzzyR needs to ensure that an environment can be 
+# formed before it can run the requisite comparison method. 
+
 #### Workspace setup ####
 library(tidyverse)
 library(here)
@@ -22,7 +26,12 @@ library(fuzzywuzzyR)
 # loaded and processed before being accessed in the main document. All images 
 # accessed will be in jpg format before being placed in /inputs/ for consistency.
 
+# Everything from here is processing images for the paper. This section will be 
+# processing items related to the first book, "Basic and Clinical Pharmacology 
+# 14th Edition" by Bertram E. Katzung
+
 # Generate Percent Difference between original text and raw OCR
+
 uc_1 <- read_file(here::here('inputs/data/Book1/b1_1_unclean.txt'))
 orig_1 <- read_file(here::here('inputs/data/Book1/b1_1_original.txt'))
 comp_1 <- SequenceMatcher$new(string1 = orig_1, string2 = uc_1)
@@ -99,3 +108,88 @@ Percent_Difference_cleaned <- c(percentdiff_c_1,percentdiff_c_2,percentdiff_c_3,
 
 Percent_Difference <- as.data.frame(cbind(Percent_Difference_Uncleaned, Percent_Difference_cleaned, Percent_Difference_Advanced), stringsAsFactors = FALSE)
 write_csv(Percent_Difference, here::here("inputs/data/book_1_percent_diff.csv"))
+
+#Everything from here is processing images for the paper. This section will be 
+# processing items related to the second book, "Life and Literature Book One 
+# Grade 7" by Thomas Nelson & Son
+
+# Generate Percent Difference between original text and raw OCR
+
+uc_6 <- read_file(here::here('inputs/data/Book2/b2_1_unclean.txt'))
+orig_6 <- read_file(here::here('inputs/data/Book2/b2_1_original.txt'))
+comp_6 <- SequenceMatcher$new(string1 = orig_6, string2 = uc_6)
+percentdiff_uc_6 <- comp_6$ratio()
+
+uc_7 <- read_file(here::here('inputs/data/Book2/b2_2_unclean.txt'))
+orig_7 <- read_file(here::here('inputs/data/Book2/b2_2_original.txt'))
+comp_7 <- SequenceMatcher$new(string1 = orig_7, string2 = uc_7)
+percentdiff_uc_7 <- comp_7$ratio()
+
+uc_8 <- read_file(here::here('inputs/data/Book2/b2_3_unclean.txt'))
+orig_8 <- read_file(here::here('inputs/data/Book2/b2_3_original.txt'))
+comp_8 <- SequenceMatcher$new(string1 = orig_8, string2 = uc_8)
+percentdiff_uc_8 <- comp_8$ratio()
+
+uc_9 <- read_file(here::here('inputs/data/Book2/b2_4_unclean.txt'))
+orig_9 <- read_file(here::here('inputs/data/Book2/b2_4_original.txt'))
+comp_9 <- SequenceMatcher$new(string1 = orig_9, string2 = uc_9)
+percentdiff_uc_9 <- comp_9$ratio()
+
+uc_10 <- read_file(here::here('inputs/data/Book2/b2_5_unclean.txt'))
+orig_10 <- read_file(here::here('inputs/data/Book2/b2_5_original.txt'))
+comp_10 <- SequenceMatcher$new(string1 = orig_10, string2 = uc_10)
+percentdiff_uc_10 <- comp_10$ratio()
+
+# Generate Percent Difference between original text and processed OCR
+
+c_6 <- read_file(here::here('inputs/data/Book2/b2_1_clean.txt'))
+comp_6a <- SequenceMatcher$new(string1 = orig_6, string2 = c_6)
+percentdiff_c_6 <- comp_6a$ratio()
+
+c_7 <- read_file(here::here('inputs/data/Book2/b2_2_clean.txt'))
+comp_7a <- SequenceMatcher$new(string1 = orig_7, string2 = c_7)
+percentdiff_c_7 <- comp_7a$ratio()
+
+c_8 <- read_file(here::here('inputs/data/Book2/b2_3_clean.txt'))
+comp_8a <- SequenceMatcher$new(string1 = orig_8, string2 = c_8)
+percentdiff_c_8 <- comp_8a$ratio()
+
+c_9 <- read_file(here::here('inputs/data/Book2/b2_4_clean.txt'))
+comp_9a <- SequenceMatcher$new(string1 = orig_9, string2 = c_9)
+percentdiff_c_9 <- comp_9a$ratio()
+
+c_10 <- read_file(here::here('inputs/data/Book2/b2_5_clean.txt'))
+comp_10a <- SequenceMatcher$new(string1 = orig_10, string2 = c_10)
+percentdiff_c_10 <- comp_10a$ratio()
+
+# Generate Percent Difference between original text and advanced processed OCR
+
+a_6 <- read_file(here::here('inputs/data/Book2/b2_1_adv.txt'))
+comp_6b <- SequenceMatcher$new(string1 = orig_6, string2 = a_6)
+percentdiff_a_6 <- comp_6b$ratio()
+
+a_7 <- read_file(here::here('inputs/data/Book2/b2_2_adv.txt'))
+comp_7b <- SequenceMatcher$new(string1 = orig_7, string2 = a_7)
+percentdiff_a_7 <- comp_7b$ratio()
+
+a_8 <- read_file(here::here('inputs/data/Book2/b2_3_adv.txt'))
+comp_8b <- SequenceMatcher$new(string1 = orig_8, string2 = a_8)
+percentdiff_a_8 <- comp_8b$ratio()
+
+a_9 <- read_file(here::here('inputs/data/Book2/b2_4_adv.txt'))
+comp_9b <- SequenceMatcher$new(string1 = orig_9, string2 = a_9)
+percentdiff_a_9 <- comp_9b$ratio()
+
+a_10 <- read_file(here::here('inputs/data/Book2/b2_5_adv.txt'))
+comp_10b <- SequenceMatcher$new(string1 = orig_10, string2 = a_10)
+percentdiff_a_10 <- comp_10b$ratio()
+
+
+# enter data into CSV
+Percent_Difference_Advanced_book2 <- c(percentdiff_a_6, percentdiff_a_7, percentdiff_a_8, percentdiff_a_9, percentdiff_a_10)
+Percent_Difference_Uncleaned_book2 <- c(percentdiff_uc_6,percentdiff_uc_7,percentdiff_uc_8,percentdiff_uc_9,percentdiff_uc_10)
+Percent_Difference_cleaned_book2 <- c(percentdiff_c_6,percentdiff_c_7,percentdiff_c_8,percentdiff_c_9,percentdiff_c_10)
+
+Percent_Difference_2 <- as.data.frame(cbind(Percent_Difference_Uncleaned_book2, Percent_Difference_cleaned_book2, Percent_Difference_Advanced_book2), stringsAsFactors = FALSE)
+write_csv(Percent_Difference_2, here::here("inputs/data/book_2_percent_diff.csv"))
+
